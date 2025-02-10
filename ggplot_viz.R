6b7dfa2b262b16b60603a50a12a9911255832205
biadamix <birhanu.adamie@gmail.com>
2025-02-10 16:03:34 +0100

    refined code and vizs

--- ggplot_viz.R
+++ ggplot_viz.R
@@ -22,7 +22,8 @@ ggplot(data = penguins, aes(flipper_length_mm, body_mass_g, colour = species)) +
        x = "Flipper Length (in mm)",
        y = "Boddy Mass (in grams)",
        colour = "Species Type"
-  )
+  ) +
+  theme_minimal()
 
 ggsave("flip_len-bod_mas_point-facet_viz.png", dpi = 300)
 
@@ -38,7 +39,8 @@ ggplot(data = penguins, aes(flipper_length_mm, body_mass_g, colour = species)) +
        y = "Boddy Mass (in grams)",
        colour = "Species Type",
        caption = "Data: 'Palmerpenguins' R package"
-  )
+  ) +
+  theme_minimal()
 
 
 # Density of Flipper length across species
@@ -46,7 +48,9 @@ ggplot(data = penguins, aes(flipper_length_mm, colour = species)) +
   geom_density()
 
 ggplot() +
-  geom_density(data = penguins, aes(flipper_length_mm, fill = species), alpha = 0.5)
+  geom_density(data = penguins, 
+               aes(flipper_length_mm, fill = species), alpha = 0.5) +
+  theme_minimal()
 
 ggsave("flip_density_viz.png", dpi = 300)
 
